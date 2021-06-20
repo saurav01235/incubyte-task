116b401299fbb9d08d48072d3b090345f78bde38
Saurav Jain <sauravjain8sj@gmail.com>
2021-06-21 01:48:17 +0530

    First commit

---
+++ src/metadata/header_type.py
@@ -0,0 +1,5 @@
+file_header = "(Customer VARCHAR(255) NOT NULL, Customer_ID VARCHAR(18) NOT NULL," \
+              " Customer_Open_Date INT(8) NOT NULL, Last_Consulted_Date INT(8), " \
+              "Vaccination_Type CHAR(5), Doctor_Consulted CHAR(255), State CHAR(255), " \
+              "Country CHAR(5), Post_Code INT(255), Date_of_Birth INT(8), " \
+              "Active_Customer CHAR(1) , PRIMARY KEY(Customer))"
